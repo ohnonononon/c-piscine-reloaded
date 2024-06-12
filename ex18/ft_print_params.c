b1d21d850eaa5e2b9d69aff1fffcaf2f3bcda2c8
Nicolás Maturana Chandia <nimatura@car3s5.42barcelona.com>
2024-06-12 20:27:45 +0200

    fix: iterates correctly, prints newline

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -6,28 +6,27 @@
 /*   By: ohnudes <nimatura@student.42barcel>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 18:53:11 by ohnudes           #+#    #+#             */
-/*   Updated: 2024/06/09 18:56:05 by ohnudes          ###   ########.fr       */
+/*   Updated: 2024/06/12 20:27:43 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_putchar(int c);
+void	ft_putchar(char c);
 
 void	aux_pstr(char *str)
 {
 	while (*str)
 		ft_putchar(*(str++));
+	ft_putchar('\n');
 }
 
 int	main(int ac, char **av)
 {
 	int	i;
-	int	j;
 
 	i = 1;
-	j = 0;
 	if (ac < 2)
 		return (0);
-	while (ac--)
+	while (av[i])
 		aux_pstr(av[i++]);
 	return (0);
 }
