23cb74c3b900efb1c150bb19019732509b5b9816
Nicolás Maturana Chandia <nimatura@car3s5.42barcelona.com>
2024-06-12 20:37:42 +0200

    fix: err message for cannot open file and nullify the string

--- ex27/display_file.c
+++ ex27/display_file.c
@@ -6,7 +6,7 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 19:42:29 by nimatura          #+#    #+#             */
-/*   Updated: 2024/06/12 19:45:31 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/06/12 20:37:13 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	print_to_output(char *buffer, int read_bytes)
 	int	i;
 
 	i = 0;
-	while (i < read_bytes)
+	while (i < read_bytes && buffer[i] != '\0')
 		ft_putchar_fd(1, buffer[i++]);
 	return (0);
 }
@@ -41,7 +41,7 @@ int	print_to_output(char *buffer, int read_bytes)
 
 int	read_and_print(int fd)
 {
-	char	buffer[1024];
+	char	buffer[1025];
 	size_t	read_bytes;
 
 	read_bytes = 1;
@@ -50,6 +50,7 @@ int	read_and_print(int fd)
 		read_bytes = read(fd, buffer, 1024);
 		if (read_bytes == -1)
 			return (err_exit(fd, "Cannot read file."));
+		buffer[read_bytes] = '\0';
 		print_to_output(buffer, read_bytes);
 	}
 	return (0);
@@ -57,19 +58,16 @@ int	read_and_print(int fd)
 
 int	main(int ac, char **av)
 {
-	char	*path;
 	int		fd;
 
-	path = NULL;
 	fd = 0;
 	if (ac == 1)
-		return (err_exit(2, "File name missing."));
+		return (err_exit(2, "File name missing.\n"));
 	if (ac > 2)
-		return (err_exit(2, "Too many arguments."));
-	path = av[1];
-	fd = open(path, O_RDONLY);
+		return (err_exit(2, "Too many arguments.\n"));
+	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-		return (err_exit(2, "Wrong path: not a valid path."));
+		return (err_exit(2, "Cannot read file.\n"));
 	if (read_and_print(fd) == -1)
 		return (0);
 	close(fd);
