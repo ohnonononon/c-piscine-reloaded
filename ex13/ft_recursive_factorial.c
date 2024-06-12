0f311bb1a7c31c466203d4866395927692e2c2d8
Nicolás Maturana Chandia <nimatura@car3s5.42barcelona.com>
2024-06-12 20:12:43 +0200

    fix: 1 - 0 cases. Gotta test

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,17 +6,15 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:23:10 by nimatura          #+#    #+#             */
-/*   Updated: 2024/06/10 17:23:26 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/06/12 20:12:39 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb == 2)
-		return (2);
-	if (nb < 1)
+	if (nb < 0)
 		return (0);
-	if (nb > 12)
-		return (0);
+	if (nb <= 1)
+		return (1);
 	return (nb * ft_recursive_factorial(nb - 1));
 }
