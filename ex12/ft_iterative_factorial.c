9ff3fb81e3c54fd21cd8a05d8d0ddeb08f2a3f49
Nicolás Maturana Chandia <nimatura@cbr3s3.42barcelona.com>
2024-06-13 15:30:48 +0200

    trying fix

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,15 +6,18 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:22:25 by nimatura          #+#    #+#             */
-/*   Updated: 2024/06/13 15:18:20 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/06/13 15:30:44 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// fixed: res was declared as long - but shouldn't
+// the value returned is an int.
+// fixed: nb > 12 returns 0
 int	ft_iterative_factorial(int nb)
 {
 	int	res;
 
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb <= 0)
 		return (1);
@@ -30,6 +33,5 @@ int	ft_iterative_factorial(int nb)
 #include <stdio.h>
 int	main(void)
 {
-	printf("%d\n", ft_iterative_factorial(1));
-	printf("%d\n", ft_iterative_factorial(0));
+	printf("%d\n", ft_iterative_factorial(12));
 }*/
