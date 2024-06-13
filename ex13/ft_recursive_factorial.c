0304903eacc9587a42234909981089d285333d6b
Nicolás Maturana Chandia <nimatura@cbr3s3.42barcelona.com>
2024-06-13 15:31:56 +0200

    fix: capped int

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:23:10 by nimatura          #+#    #+#             */
-/*   Updated: 2024/06/12 20:12:39 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/06/13 15:31:54 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb > 12)
 		return (0);
 	if (nb <= 1)
 		return (1);
