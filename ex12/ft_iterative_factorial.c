27ab7f2462d7ae82f93cef2e046b97bc971ca3a6
Nicolás Maturana Chandia <nimatura@cbr3s3.42barcelona.com>
2024-06-13 15:31:21 +0200

    fix: nb <= 1

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: nimatura <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:22:25 by nimatura          #+#    #+#             */
-/*   Updated: 2024/06/13 15:30:44 by nimatura         ###   ########.fr       */
+/*   Updated: 2024/06/13 15:31:10 by nimatura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_iterative_factorial(int nb)
 
 	if (nb < 0 || nb > 12)
 		return (0);
-	if (nb <= 0)
+	if (nb <= 1)
 		return (1);
 	res = 1;
 	while (nb >= 1)
